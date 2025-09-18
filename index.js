e8ef5a0df60e43811ce76e9655affeb64bb7d1d2
JoseCarlos <josecarlosfernandezperez9@gmail.com>
2025-09-18 08:16:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-// https://ilp.interledger-test.dev/bostest Cliente
-// https://ilp.interledger-test.dev/johana Remitente
-// https://ilp.interledger-test.dev/josec Receptor
-
-// Tutorial del cliente de Open Payments
-// Objetivo: Realizar un pago entre pares entre dos direcciones de billetera (usando cuentas en la cuenta de prueba)
-//
-// ConfiguraciÃ³n inicial
-
 import { createAuthenticatedClient} from "@interledger/open-payments";
 import  fs  from "fs";
 import { isFinalizedGrant } from "@interledger/open-payments";
+import  Readline  from "readline/promises";
+
+function Cliente() {
+  let servicio = document.getElementById ("servicio").value;
+  let monto = document.getElementById ("paymet-amount").value;
+  const moneda = parseFloat(monto.value);
+  let wallet = document.getElementById ("receiver-address").value;
+
+// Objetivo: Realizar un pago entre pares entre dos direcciones de billetera (usando cuentas en la cuenta de prueba)
+// ConfiguraciÃ³n inicial
 
 // a. Importar dependencias y configurar el cliente
 
@@ -21,32 +22,22 @@ import { isFinalizedGrant } from "@interledger/open-payments";
     keyId: "56a7a7c4-ff03-4753-884c-d1208515c866",
         });
 
- // b. Crear una instancia del cliente Open Payments
-
-
-
-    // c. Cargar la clave privada del archivo
-
-
-    // d. Configurar las direcciones de las billeteras del remitente y el receptor
-
-
-
-    // Flujo de pago entre pares
-
-
-
 // 1. Obtener una concesiÃ³n para un pago entrante)
 
 const sendingWalletAddress = await client.walletAddress.get({
-    url:"https://ilp.interledger-test.dev/johana"
-        } );
 
+        //la del que paga (cliente/usuario).
+    url:'https://' + wallet.value
+        } ); 
+
+        //la del que cobra (empresa/servicio).
     const receivingwalletAddress = await client.walletAddress.get({
-    url: "https://ilp.interledger-test.dev/josec"
+    url: "https://ilp.interledger-test.dev/jjumb"
         } );
 
      console.log(sendingWalletAddress, receivingwalletAddress);
+
+
 
 // 2. Obtener una concesiÃ³n para un pago entrante
 
@@ -88,7 +79,7 @@ const sendingWalletAddress = await client.walletAddress.get({
       incomingAmount: {
         assetCode: receivingwalletAddress.assetCode,
         assetScale: receivingwalletAddress.assetScale,
-        value: "10",
+        value: "moneda.value",
       },
     }
   );
@@ -137,9 +128,79 @@ console.log({ quote });
 
 // 6. Obtener una concesiÃ³n para un pago saliente
 
-const outgoingPaymentGrant =
+const outgoingPaymentGrant = await client.grant.request(
+  {
+    url: sendingWalletAddress.authServer,
+  },
+  {
+    access_token: {
+      access:[
+        {
+          type:"outgoing-payment",
+          actions:["create"],
+          limits: {
+            debitAmount:quote.debitAmount,
+          },
+          identifier: sendingWalletAddress.id,
+        }
+      ]
+    },
+    interact:{
+     start: ["redirect"],
+    },
+  }
+);
+console.log(outgoingPaymentGrant.interact);
+
+return res.json({
+  redirectUrl: outgoingPaymentGrant.interact.redirect, // <-- ESTA ES LA URL QUE DEBES ABRIR
+  continueUri: outgoingPaymentGrant.continue.uri, // para después continuar el flujo
+  continueToken: outgoingPaymentGrant.continue.access_token.value
+});
+
+async function confirmarPago() {
+  const response = await fetch("/api/crear-pago");
+  const data = await response.json();
+
+  if (data.redirectUrl) {
+    // Redirige al usuario al servidor de autorización
+    window.location.href = data.redirectUrl;
+  }
+}
 
 // 7. Continuar con la concesiÃ³n del pago saliente
+
+await Readline
+.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+.question("presione Enter para continuar con el pago saliente...");
+
 // 8. Finalizar la concesiÃ³n del pago saliente
+
+const finalizedOutgoingPaymentGrant= await client.grant.continue({
+  url: outgoingPaymentGrant.continue.uri,
+  accessToken: outgoingPaymentGrant.continue.access_token.value,
+});
+if ( !isFinalizedGrant(finalizedOutgoingPaymentGrant)){
+  throw new Error("Se espera finalice la concesion");
+}
+
 // 9. Continuar con la cotizaciÃ³n de pago saliente
+
+const outgoingPayment = await client.outgoingPayment.create(
+  {
+    url: sendingWalletAddress.resourceServer,
+    accessToken: finalizedOutgoingPaymentGrant.access_token.value,
+  },
+  {
+    walletAddress: sendingWalletAddress.id,
+    quoteId: quote.id,
+  }
+);
+
+console.log({ outgoingPayment});
+
 })();
+}
